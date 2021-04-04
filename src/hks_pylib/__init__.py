a3bf53373d046b778f8caafb0d6f519523f88363
Le Huy <huykingsofm@gmail.com>
2021-04-04 17:33:41 +0700

    Modify README in hks_pylib __init__

--- src/hks_pylib/__init__.py
+++ src/hks_pylib/__init__.py
@@ -2,17 +2,21 @@
 hks_pylib
 ==============
 
-This is a Python 3 utility library of `huykingsofm` (https://www.github.com/huykingsofm).
+This is a Python 3 utility library of `huykingsofm` (https://github.com/huykingsofm).
 It has some modules, including:
-- `logger`: A module is used to print notifications to console screen or write logs to file.
-It is special because you can disable the print/write statement by modifying few parameters
-without having to delete or comment them manually. 
-- `cipher`: A very simple crypto module based on `cryptography`
-(https://pypi.org/project/cryptography). It is easier to use than the original one.
-- `done`: A module defines a class (`Done`) for returning complex values more convenient.
-- `http`: A module is used to read and create raw http packets.
+- `logger`: A module is used to print notifications to the 
+console screen or write logs to file. It is special because 
+you can disable the print/write statement by modifying few
+parameters without having to delete or comment them manually. 
+- `cipher`: A very simple crypto module based on
+`cryptography` (https://pypi.org/project/cryptography/).
+It is easier to use than the original one.
+- `done`: A module defines a class (`Done`) for returning
+complex values more conveniently.
+- `http`: A module is used to parse or generate raw http
+packets.
 """
-import hks_pylib
+
 import hks_pylib.cipher as _cipher
 from hks_pylib.version import __version__
 
